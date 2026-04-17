ef1dbf7b62e9d3cb40101e319c22eca77c18f0e5
adrianodes <adriano_gon.leite@hotmail.com>
2026-04-16 22:07:41 -0300

    nova pasta

---
+++ 01-javascript/testes.js
@@ -0,0 +1,24 @@
+// alert("Olá mundo!")
+// alert("SEGUNDA MENSAGEM")
+
+let nome
+
+nome 
+
+// alert("Nome: " + nome)
+
+let idade = 33
+// alert("idade: " + idade)
+
+// alert("Nome: " + nome + "\nIdade: " + idade)
+
+// nome = prompt("Digite seu nome: ") 
+// alert("Nome digitado: " + nome)
+
+
+let nota1
+
+nota1 = prompt("Digite primeira nota: ")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert("Nota digitada: " + nota1)
