d166120121e874c24d28f157981decb463294842
adrianodes <adriano_gon.leite@hotmail.com>
2026-04-23 22:14:14 -0300

    trajeto pomar

---
+++ 01-javascript/guilhermePortoes.js
@@ -0,0 +1,11 @@
+//informações
+let clt, estagiarios, pj, total
+//leitura
+clt = Number(prompt("Funcionários CLT: "))
+estagiarios = Number(prompt("Estagiários: "))
+pj = Number(prompt("Funcionários PJ: "))
+//processamentos
+total = clt + estagiarios + pj
+//saídas
+console.log("Total de DEVS: " + total)
+
