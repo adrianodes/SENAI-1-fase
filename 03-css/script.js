48ca3f52ad6a2015aa81b509c968169f596d3fe2
adrianodes <adriano_gon.leite@hotmail.com>
2026-04-30 22:27:35 -0300

    css

--- 03-css/script.js
+++ 03-css/script.js
@@ -1,4 +1,4 @@
-
+/// Preço Brique
 function calcularPrecoBrique(){
 // INFO
 let precoCompra, precoVenda
@@ -13,7 +13,8 @@ alert("Preço de venda: R$" + precoVenda.toFixed(2))
 document.getElementById("resultado").innerHTML = "Preço de venda: R$"
 + precoVenda.toFixed(2)
 }
-
+//==============================================================================
+// Monika
 function porcentagemChance(){
 //apresentações
 let chance, n
@@ -27,4 +28,54 @@ console.log("Percentual de aprovação: " + chance.toFixed(4))
 
 document.getElementById("chance").innerHTML = "Percentual de aprovação: "
 + chance.toFixed(4) + "%"
+}
+//=================================================================================
+// Capitão Ganso no mar
+function verificarProvisoes(){
+    let marujos, comida
+    let comidaPorMarujo
+    // entradas
+    marujos = Number(prompt("Quantidade de marujos: "))
+    comida = Number(prompt("Quilos de comida"))
+    // processamentos
+    comidaPorMarujo = comida / marujos
+    // saídas
+    if(marujos >= 10 && comidaPorMarujo >= 1.5){ // ||
+        document.getElementById("resultado").innerHTML = "provisões suficientes rumo ao horizonte!"
+}else{
+    document.getElementById("resultado").innerHTML = "Algo está errado Posseidom não quer ninguem no mar hoje."
+}
+}
+//=============================================================================
+// Bingo dana Bete
+function donaBeteFaturamento(){
+    //apresentações
+let totalBruto, premiacoes, presentes, comissoes, lucro
+//leituras
+totalBruto = Number(prompt("Total bruto: R$"))
+premiacoes = Number(prompt("Premiações: R$"))
+presentes = Number(prompt("Presentes: R$"))
+comissoes = Number(prompt("Comissões: R$"))
+//calculos
+lucro = totalBruto - premiacoes - presentes - comissoes
+// saídas
+console.log("Lucro do dia: R$" + lucro.toFixed(2))
+document.getElementById("lucro").innerHTML = "Lucro do dia: R$"
++ lucro.toFixed(2)
+}
+//=========================================================================
+// Pé pequeno calçados
+
+function pePequenoCaçados(){
+    // informações
+let preco, quantidade
+let valorTotal
+// leitura
+quantidade = Number(prompt("Quantidade:"))
+preco = Number(prompt("preco: R$"))
+// processamentos
+valorTotal = quantidade * preco
+// Saídas
+console.log("Valor a receber: R$" + valorTotal.toFixed(2))
+document.getElementById("valorTotal").innerHTML = "Valor a receber: R$" + valorTotal.toFixed(2)
 }
